0ce27ad0b1236105e9924ce532d96693a5d856e9
adekore1 <adekorebalogun@cmail.carleton.ca>
2025-10-23 13:04:49 -0400

    Update FileExplorer.tsx

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -68,7 +68,9 @@ export default function FileExplorer() {
           }}
           onClick={() => fileInputRef.current?.click()}
         >
-          <p className="text-sm">Drag & drop a FOLDER here</p>
+          <p className="text-sm">Drag & drop a FOLDER here
+            *must not be corrupt*
+          </p>
           <p className="text-indigo-500 underline text-sm">
             or click to select one
           </p>
