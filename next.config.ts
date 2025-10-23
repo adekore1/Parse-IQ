7f02c1cd51dd5ab3e81aeb5b72ced02af9f817e6
adekore1 <adekorebalogun@cmail.carleton.ca>
2025-10-23 14:43:43 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), {
-        canvas: 'commonjs canvas',
-      }];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
